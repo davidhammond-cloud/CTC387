caf8dc83c57fd1c7f86bc2859855b1b6e7e7deda
dhammond5 <dhammond5@csudh>
2026-03-19 01:40:14 +0000

    2nd try programreview.py

---
+++ programreview1.py
@@ -0,0 +1,6 @@
+number1 =int(input ("Enter number1 (1 through 10): "))
+number2 =int(input ("Enter number2 (1 through 10): "))
+number3 =int(input ("Enter number3 (1 through 10): "))
+number4 =int(input ("Enter number4 (1 through 10): "))
+product = number1 * number2 * number3 * number4
+print ("The product of your numbers is: ", product)
