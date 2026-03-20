12d58a1f4e5dca50e1d48eb78e7ebda77fa47fbd
dhammond5 <dhammond5@csudh>
2026-03-20 03:28:48 +0000

    what will the raccoon say?

---
+++ raccoonsay.py
@@ -0,0 +1,14 @@
+def raccoon_say(message):
+    print ("Raccoon says: ")
+    print (message)
+
+    print ("        (\\_/)")
+    print ("        (o.o)")
+    print ("        /    \\")
+    print ("       (|    |)")
+    print ("      /| |_| | \\" )
+    print ("     /_/     \\_\\")
+
+user_input = input ("Enter a message: ")
+raccoon_say(user_input)
+
